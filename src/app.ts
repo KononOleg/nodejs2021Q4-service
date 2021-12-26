338d8b3046a4b4378b4d3bb71cadef8ae4be321f
KononOleg <konon.oleg97@gmail.com>
2021-12-26 15:56:25 +0300

    feat: add logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import  fastify  from 'fastify'
-import userRouter  from './resources/users/user.router';
-import boardRouter  from './resources/board/board.router';
-import taskRouter  from './resources/tasks/task.router';
+import fastify from 'fastify';
+import userRouter from './resources/users/user.router';
+import boardRouter from './resources/board/board.router';
+import taskRouter from './resources/tasks/task.router';
+import { loggerOptions, logger } from './logger';
 
-export const app = fastify();
+export const app = fastify(loggerOptions);
 
+app.register(logger);
 app.register(userRouter, { prefix: '/users' });
 app.register(boardRouter, { prefix: '/boards' });
 app.register(taskRouter, { prefix: '/boards/:boardId/tasks' });
---
+++ src/logger.ts
@@ -0,0 +1,49 @@
+import {
+  FastifyInstance,
+  FastifyLoggerOptions,
+  FastifyServerOptions,
+} from 'fastify';
+import { app } from './app';
+
+const loggerOptions = {
+  logger: {
+    prettyPrint: {
+      colorize: false,
+      ignore: 'pid,hostname,reqId,responseTime',
+      translateTime: true,
+    },
+  },
+  disableRequestLogging: true,
+} as FastifyLoggerOptions;
+
+const requestLogging = (): void => {
+  app.addHook('preHandler', (req, _reply, done) => {
+    const { method, url, params, body, query } = req;
+    req.log.info(
+      `Received ${method} request to ${url} with params: ${JSON.stringify(
+        params
+      )}, query: ${JSON.stringify(query)}, body: ${JSON.stringify(body)} `
+    );
+    done();
+  });
+};
+
+const responseLogging = (): void => {
+  app.addHook('onResponse', (_res, reply, done) => {
+    const { statusCode } = reply;
+    reply.log.info(`Request completed with ${statusCode} status code`);
+    done();
+  });
+};
+
+const logger = (
+  _fastify: FastifyInstance,
+  _opts: FastifyServerOptions,
+  done: () => void
+): void => {
+  requestLogging();
+  responseLogging();
+  done();
+};
+
+export { loggerOptions, logger };
