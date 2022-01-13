2aa27208f153a4a11a717a3f2ab0aa5fcfc4f456
KononOleg <konon.oleg97@gmail.com>
2022-01-13 16:44:46 +0300

    feat: add database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,19 @@
 import config from './common/config';
 import { app } from './app';
+import { createConnection } from 'typeorm';
 
-app.listen((config.PORT = '4000'));
+createConnection({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '1234',
+  database: 'database',
+  synchronize: true,
+})
+  .then(async () => {
+    app.listen((config.PORT = '4000'));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
